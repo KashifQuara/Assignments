f379e36a9c92af802c14de000725763aece8df4e
KashifQuara <125881180+KashifQuara@users.noreply.github.com>
2023-02-23 17:25:02 +0530

    Update Assignment leapyear.js

--- Assignment leapyear.js
+++ Assignment leapyear.js
@@ -1,8 +1,12 @@
 function checkLeapYear() {
-    let year = Number(prompt("Enter a year:"));
-    let nextLeapYear = year + (4 - (year % 4));
-    let message = (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) ? `${year} is a leap year` : `${year} is not a leap year. After ${year}, ${nextLeapYear} is the next leap year.`;
-    console.log(message);
+  let year = Number(prompt("Enter a year:"));
+  if (!Number.isInteger(year)) {
+    console.log("Please enter a whole number for the year.");
+    return;
   }
-  
-  checkLeapYear()
+  let nextLeapYear = year + (4 - (year % 4));
+  let message = (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) ? `${year} is a leap year` : `${year} is not a leap year. After ${year}, ${nextLeapYear} is the next leap year.`;
+  console.log(message);
+}
+
+checkLeapYear();
