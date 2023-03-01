4dfdb794f33077369fb2075799c8d3b6c71f7228
KashifQuara <125881180+KashifQuara@users.noreply.github.com>
2023-03-01 14:15:07 +0530

    Add files via upload

---
+++ sysinfo.js
@@ -0,0 +1,16 @@
+const os = require('os');
+console.log('Operating system:', os.type());
+console.log('Release:', os.release());
+console.log('Current User:', os.userInfo());
+console.log('CPU Architecture:', os.arch());
+console.log('Total Memory (bytes):', os.totalmem());
+console.log('Free Memory (bytes):', os.freemem());
+console.log('Home Directory:', os.homedir());
+console.log('Temp Directory:', os.tmpdir());
+console.log('Host Name:', os.hostname());
+
+
+
+
+
+
